ff93278a9867427424fb19549800a5b4e757d3b1
Vakada Kushal <157359800+Kushal-796@users.noreply.github.com>
2026-04-11 11:13:44 +0530

    task-3-done
    Refactor main function and update string handling

--- task3.c
+++ task3.c
@@ -1,53 +1,44 @@
-#include<stdio.h>
-#include<ctype.h>
-#include<stdlib.h>
-#include<string.h>
-#include<math.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <math.h>
 
-void main()
-{
-    int i = 0, n;
-    void * p, * add[5]; // because these store addresses
-    char ch, srch, b[15], d[15], c;
+void main(){
+    printf("Enter string ending with $: \n");
     
+    char s[20], ch;
+    int i=0, j=0, k=0;
+    void *p, *addr[5];
+    char c, d[5];
     
-    while ((c = getchar()) != '$'){
-        b[i++] = c;
+    while((ch=getchar())!='$'){
+        s[i++]=ch;
     }
     
-    n = i - 1;
-    printf("Given Expression:");
-    i = 0;
+    int n=i-1;
     
-    while (i <= n){
-        printf("%c", b[i++]);
-    }
+    printf("Symbol\tAddr\tType\n");
     
-    printf("\n Symbol Table\n");
-    printf("Symbol \t addr \t type");
-    
-    int j=0, x=0;
-    
-    while (j <= n){
-        c = b[j];
-        if (isalpha(c)){
-            p = malloc(c);
-            add[x] = p;
-            d[x] = c;
-            printf("\n%c \t %d \t identifier\n", c, p);
-            x++;
+    while(j<=n){
+        ch=s[j];
+        if(isalpha(ch)){
+            p=malloc(ch);
+            addr[k]=p;
+            d[k]=ch;
+            printf("%c\t%d\tIdentifier\n", ch, p);
+            k++;
             j++;
-        }
-        else{
-            ch = c;
-            if (ch == '+' || ch == '-' || ch == '*' || ch == '='){
-                p = malloc(ch);
-                add[x] = p;
-                d[x] = ch;
-                printf("\n %c \t %d \t operator\n", ch, p);
-                x++;
+        }else{
+            if(strchr("+-*/", ch)){
+                p=malloc(ch);
+                addr[k]=p;
+                d[k]=ch;
+                printf("%c\t%d\tOperator\n", ch, p);
+                k++;
                 j++;
             }
         }
     }
+    
 }
